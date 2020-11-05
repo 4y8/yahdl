06c2f08afafff356a6663196e1a05840be70a57c
Yul3n <yul3n.falx@protonmail.com>
2020-11-05 18:12:42 +0100

    Continued.

--- src/gram.c
+++ src/gram.c
@@ -2,4 +2,16 @@
 
 #include "gram.h"
 
+static struct node mknide(char *);
+static struct node mkngate(char *, int, struct node *);
+static struct decl mkdecl(char *, int, struct node *);
+
 static struct node get_node(void);
+
+struct decl *program(void);
+
+
+static struct node mknide(char *name)
+{
+	return (struct node){.type = N_IDE, .name = name};
+}
