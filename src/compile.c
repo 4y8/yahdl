733e053f5502e4afcab0a4fa0da480123155cab9
Yul3n <yul3n.falx@protonmail.com>
2020-11-14 14:10:06 +0100

    Continued.

--- src/compile.c
+++ src/compile.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "compile.h"
 
 static void append_env(struct env *, char *);
-static struct decl_ir *to_ir(int, struct decl *, int *);
+static int find_env(struct env, char *);
+
+static struct ir node_to_ir(struct node, struct env);
+static struct decl_ir *decls_to_ir(int, struct decl *);
 static short *compile_ir(int, struct ir *);
 
 short *compile(int, struct decl *);
@@ -16,16 +20,49 @@ append_env(struct env *env, char *e)
 	*(env->elems + env->len - 1) = e;
 }
 
-static struct decl_ir *
-to_ir(int inlen, struct decl *decls, int *outlen)
+static int
+find_env(struct env env, char *s)
 {
-	for (int i = 0; i < inlen; ++i) {
+	for (int i = 0; i < env.len; ++i)
+		if (!strcmp(s, env.elems[i]))
+			return i;
+
+	return -1;
+}
+
+static struct ir
+node_to_ir(struct node n, struct env env)
+{
+	switch (n.type) {
+	case N_IDE:
+		return (struct ir){.type = IR_STACK, .n = find_env(env, n.name)};
+
+	case N_GATE:
+		break;
+	}
+}
+
+static struct decl_ir *
+decls_to_ir(int len, struct decl *decls)
+{
+	struct decl_ir *ir_decls;
+	struct decl     decl;
+
+	ir_decls = malloc(len * sizeof(struct decl_ir));
+	for (int i = 0; i < len; ++i) {
+		decl        = decls[i];
+		ir_decls[i] = (struct decl_ir){
+			.body = malloc(decl.size + 1),
+			.size = decl.size + 1
+		};
 		struct env env = {.len   = 0,
 			          .elems = malloc(sizeof(char *))};
 		for (int j = 0; j < decls[i].narg; ++j)
-			append_env(&env, decls[i].args[j]);
-		for (int j = 0; j < decls[i].size; ++j) {
+			append_env(&env, decl.args[j]);
+		for (int j = 0; j < decl.size; ++j) {
+			ir_decls[i].body[j] = node_to_ir(decl.body[j].node, env);
+			append_env(&env, decl.body[j].node.name);
 		}
-		--inlen;
 	}
+	return ir_decls;
 }
