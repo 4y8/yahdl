632cc41c1e954396f89887d7df7327cc4811f4d3
Yul3n <yul3n.falx@protonmail.com>
2020-11-22 11:45:46 +0100

    Continued.

--- src/debug.c
+++ src/debug.c
@@ -13,3 +13,40 @@ void show_decl_ir(struct decl_ir *);
 void show_asm(short *);
 
 void show_list(void (*)(void *), int, void *);
+
+void
+show_token(struct token *t)
+{
+}
+
+void
+show_node(struct node *n)
+{
+}
+
+void
+show_decl(struct decl *d)
+{
+}
+
+void
+show_ir(struct ir *i)
+{
+}
+
+void
+show_decl_ir(struct decl_ir *d)
+{
+}
+
+void
+show_asm(short *s)
+{
+}
+
+void
+show_list(void (*f)(void *), int n, void *l)
+{
+	for (int i = 0; i < n; ++i)
+		f(l + i);
+}
